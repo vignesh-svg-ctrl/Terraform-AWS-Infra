fa56d184471b225f274e7d590013aff2ef69ab22
vignesh-svg-ctrl <vigneshcareer7@gmail.com>
2026-01-21 22:13:27 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,19 @@
-cat > index.html <<EOF
-<h1>Hello, World</h1>
-<p>Welcome to my Terraform-provisioned web server</p>
+#!/bin/bash
+set -euxo pipefail
+
+dnf install -y httpd
+
+systemctl enable httpd
+systemctl start httpd
+
+cat <<EOF > /var/www/html/index.html
+<html>
+  <head>
+    <title>Terraform 2-Tier Architecture</title>
+  </head>
+  <body>
+    <h1>Hello from Private EC2</h1>
+    <p>Served via ALB + NAT Gateway</p>
+  </body>
+</html>
 EOF
