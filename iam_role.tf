b29a2a3e1997c4aecb61d56259ed95cf881b2be0
vignesh-svg-ctrl <vigneshcareer7@gmail.com>
2026-01-21 22:14:05 +0530

    Add files via upload

---
+++ iam_role.tf
@@ -0,0 +1,24 @@
+# IAM Role for EC2 to access SSM
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "${var.project}-ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.project}-ec2-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
